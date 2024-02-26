f6719923b9a9b7dbcac565d938243e2510528dee
Yves Chalant <ychalant@mi-c2-p4.s19.be>
2024-02-26 17:16:31 +0100

    fixed memory freeing issues

--- src/map.c
+++ src/map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alexphil <alexphil@student.s19.be>         +#+  +:+       +#+        */
+/*   By: ychalant <ychalant@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 14:24:32 by alexphil          #+#    #+#             */
-/*   Updated: 2024/02/26 11:47:59 by alexphil         ###   ########.fr       */
+/*   Updated: 2024/02/26 17:15:20 by ychalant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	init_import(t_import *import, t_minirt *minirt, char *map)
 
 int	check_scene(t_import *import)
 {
-	if (!import->ambient || !import->camera || import->light)
+	if (!import->ambient || !import->camera || !import->light)
 		return (err("Scene needs one of each: Ambient, Camera and Light."));
 	return (0);
 }
@@ -61,7 +61,6 @@ int	import_map(t_minirt *minirt, char **av)
 		return (1);
 	if (read_map(&import))
 		return (1);
-	return (printf("Good map!\n"), 0);
 	if (check_scene(&import))
 		return (1);
 	return (0);
